25b6ef6c9b7dd9f52cc0a0aa6ced4d32f8514471
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-04 14:03:11 +0600

    getting error

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.txuvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('itemDatabase').collection('item');
+
+        console.log('server connected')
+    }
+    finally {
+        await client.close();
+    }
+};
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello. warehouse-management-server-side is working!')
