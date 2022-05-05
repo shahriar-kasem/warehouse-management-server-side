e5b700ce2e5bf942f9258fe5cdbce4bd94fe0025
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-05 19:30:24 +0600

    more items connected with server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,11 +25,23 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+        app.get('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const item = await itemCollection.findOne(query);
+            res.send(item)
+        })
         app.get('/service', async(req,res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        })
+        app.get('/service/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
         app.get('/feedback', async(req,res) => {
             const query = {};
