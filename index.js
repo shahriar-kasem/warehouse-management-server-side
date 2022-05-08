6b0fbc0fa3316e51e6d285a2450d16ac42c4060f
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-08 14:48:35 +0600

    trying to fixing server error

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { send } = require('express/lib/response');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,22 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
-// JWT
-function verifyJWT(req, res, next){
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send({message: 'unauthorized access'})
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err){
-            return res.status(403).send({message: 'Forbidden access'});
-        }
-        req.decoded = decoded;
-        next()
-    })
-} 
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.txuvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -46,6 +29,7 @@ async function run() {
                 expiresIn: '1d'
             });
             res.send({accessToken});
+            console.log(accessToken)
         })
 
         // get
@@ -55,18 +39,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
-        app.get('/myitem', verifyJWT, async(req, res) => {
-            const decodedEmail = req.decoded.email;
+        app.get('/myitem', async(req, res) => {
             const email = req.query.email;
-            if(email === decodedEmail){
-                const query = {email};
+            const query = {email};
             const cursor = itemCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
-            }
-            else{
-                res.status(403).send({message: 'Forbidden access'});
-            }
         })
         app.get('/inventory/:id', async(req, res) => {
             const id = req.params.id;
@@ -149,6 +127,7 @@ async function run() {
             const result = await itemCollection.deleteOne(query);
             res.send(result);
         })
+
         console.log('server connected')
     }
     finally {
